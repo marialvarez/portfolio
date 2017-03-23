ebfe5f29e959f51fc682bd8c90ff25ba06ec8bb4
Maricela <malvarezdrobush@gmail.com>
2017-03-23 18:58:24 -0500

    header done

--- js/main.js
+++ js/main.js
@@ -9,11 +9,12 @@ function init()
     $('#list-games').on('click','button', onClickBtnItemGame);
     $('#btn-comments').click(onClickBtnComments);
     */
-	//TweenMax.to($('header h1'), 3, {scale:3,force3D:false});
-    
-    //TweenMax.from($('header img'), 1, {marginBottom:'0px', ease:Elastic.easeOut});
     
     //TweenMax.to($('header img'), 3, {scale:3,force3D:false});  
     //TweenMax.from($('header img'), 1, {scale:3,force3D:false});
-    TweenMax.to("header img", 2, {x:80, y:50, ease:Sine.easeOut});
+    
+    TweenMax.to("header img:nth-child(1)", 1.2, {x:200, y:200, ease:Sine.easeOut});
+    TweenMax.to("header img:nth-child(2)", 1.2, {x:-200, y:200, ease:Sine.easeOut});
+    TweenMax.to("header img:nth-child(3)", 1.2, {x:150, y:-230, ease:Sine.easeOut});
+    TweenMax.to("header img:nth-child(4)", 1.2, {x:-150, y:-230, ease:Sine.easeOut});
 }
